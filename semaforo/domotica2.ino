a273bd5e50a73a4087df3ea2b3890265b31d2e3f
andradew <wilson00749@gmail.com>
2024-09-05 17:27:37 -0500

    refactor if in loop, use ternary operator

--- semaforo/domotica2.ino
+++ semaforo/domotica2.ino
@@ -4,38 +4,40 @@ const int redLed1 = 12;
 const int blueLed1 = 11;
 const int greenLed1 = 10;
 
-const int interruptor = 13;
-int mode;
+const int mode = 13;
 
 void setup()
 {
     pinMode(redLed1, OUTPUT);
     pinMode(blueLed1, OUTPUT);
     pinMode(greenLed1, OUTPUT);
-    pinMode(interruptor, INPUT);
+    pinMode(mode, INPUT);
 }
 
 void loop()
 {
-    mode = digitalRead(interruptor);
-    if (mode == HIGH)
-    {
-        analogWrite(redLed1, 255);
-        analogWrite(blueLed1, 0);
-        analogWrite(greenLed1, 255);
+    (digitalRead(mode) == HIGH) ? normalState() : yellowState();
+}
 
-        delay(500); // Wait for 1000 millisecond(s)
+void normalState()
+{
+    analogWrite(redLed1, 1);
+    analogWrite(blueLed1, 1);
+    analogWrite(greenLed1, 255);
+}
 
-        analogWrite(redLed1, 0);
-        analogWrite(blueLed1, 0);
-        analogWrite(greenLed1, 0);
+void yellowState()
+{
 
-        delay(500); // Wait for 1000 millisecond(s)
-    }
-    else
-    {
-        analogWrite(redLed1, 1);
-        analogWrite(blueLed1, 1);
-        analogWrite(greenLed1, 255);
-    }
+    analogWrite(redLed1, 255);
+    analogWrite(blueLed1, 0);
+    analogWrite(greenLed1, 255);
+
+    delay(500);
+
+    analogWrite(redLed1, 0);
+    analogWrite(blueLed1, 0);
+    analogWrite(greenLed1, 0);
+
+    delay(500);
 }
