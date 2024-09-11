cc4291b1bea2e73e6ce3c5b711e83367fb310e37
andradew <wilson00749@gmail.com>
2024-09-11 17:20:59 -0500

    Add requirements 2, 3 and 4

--- semaforo/domotica2.ino
+++ semaforo/domotica2.ino
@@ -1,16 +1,18 @@
 // C++ code
 
-const int redLed1 = 12;
-const int blueLed1 = 11;
-const int greenLed1 = 10;
+// red, green, blue
+const int led1[] = {12, 11, 10};
+const int led2[] = {9, 8, 7};
+const int led3[] = {6, 5, 4};
 
 const int mode = 13;
 
 void setup()
 {
-    pinMode(redLed1, OUTPUT);
-    pinMode(blueLed1, OUTPUT);
-    pinMode(greenLed1, OUTPUT);
+    setPinModeOutput(led1);
+    setPinModeOutput(led2);
+    setPinModeOutput(led3);
+
     pinMode(mode, INPUT);
 }
 
@@ -19,25 +21,83 @@ void loop()
     (digitalRead(mode) == HIGH) ? normalState() : yellowState();
 }
 
-void normalState()
-{
-    analogWrite(redLed1, 1);
-    analogWrite(blueLed1, 1);
-    analogWrite(greenLed1, 255);
-}
-
 void yellowState()
 {
-
-    analogWrite(redLed1, 255);
-    analogWrite(blueLed1, 0);
-    analogWrite(greenLed1, 255);
-
+    setAllLedsColor('y');
     delay(500);
 
-    analogWrite(redLed1, 0);
-    analogWrite(blueLed1, 0);
-    analogWrite(greenLed1, 0);
-
+    setAllLedsColor('0');
     delay(500);
+}
+
+void normalState()
+{
+    setLedColors(led1, 'g');
+    setLedColors(led2, 'r');
+    setLedColors(led3, 'r');
+    delay(2000);
+
+    setLedColors(led1, 'y');
+    setLedColors(led2, 'y');
+    setLedColors(led3, 'r');
+    delay(500);
+
+    setLedColors(led1, 'r');
+    setLedColors(led2, 'g');
+    setLedColors(led3, 'r');
+    delay(2000);
+
+    setLedColors(led1, 'r');
+    setLedColors(led2, 'y');
+    setLedColors(led3, 'y');
+    delay(500);
+
+    setLedColors(led1, 'r');
+    setLedColors(led2, 'r');
+    setLedColors(led3, 'g');
+    delay(2000);
+
+    setLedColors(led1, 'y');
+    setLedColors(led2, 'r');
+    setLedColors(led3, 'y');
+    delay(500);
+}
+
+void setPinModeOutput(const int led[])
+{
+    pinMode(led[0], OUTPUT);
+    pinMode(led[1], OUTPUT);
+    pinMode(led[2], OUTPUT);
+}
+
+void setLedColors(const int led[], char color)
+{
+    int red = 0, green = 0, blue = 0;
+
+    switch (color)
+    {
+    case 'r':
+        red = 255;
+        break;
+    case 'g':
+        blue = 255;
+        break;
+    case 'y':
+        red = 255;
+        blue = 255;
+        break;
+    case '0':
+        break;
+    }
+
+    analogWrite(led[0], red);
+    analogWrite(led[1], green);
+    analogWrite(led[2], blue);
+}
+
+void setAllLedsColor(char color)
+{
+    setLedColors(led1, color);
+    setLedColors(led2, color);
+    setLedColors(led3, color);
 }
